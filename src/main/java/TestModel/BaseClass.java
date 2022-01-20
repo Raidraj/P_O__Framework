f884e4caa320dbfc1683a8001685a369e5cfd5ba
Raidraj <raid.rajib@gmail.com>
2022-01-20 01:37:32 -0500

    class-21-2021

---
+++ src/main/java/TestModel/BaseClass.java
@@ -0,0 +1,34 @@
+package TestModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Test
+public class BaseClass {
+
+public static WebDriver driver;
+public static String BaseUrl;
+
+@BeforeSuite
+public void config() {
+ 	
+WebDriverManager.chromedriver().setup();
+//WebDriverManager.firefoxdriver().setup();
+driver = new ChromeDriver();
+//driver = new FirefoxDriver();
+BaseUrl = "http://automationpractice.com/index.php";
+	
+}
+
+@AfterSuite
+public void teardown() {
+	driver.quit();
+	
+}
+	
+}
